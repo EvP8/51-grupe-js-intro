f819623dae83f87aa60e0a4fb8ccf2c4effafacc
Evaldas Petkus <evaldas.db@gmail.com>
2024-09-18 21:01:27 +0300

    cw

--- CodeWars/cw.js
+++ CodeWars/cw.js
@@ -763,3 +763,68 @@
 // console.log(createArray(3)); 
 
 // --------------------------------------------------------
+
+// function buildString(...template) {
+//   return `I like ${template.join(', ')}!`;
+// }
+
+// console.log(buildString('Cheese', 'Milk', 'Chocolate'));
+
+// const buildString = (...template) => `I like ${template.join(', ')}!`;
+
+// --------------------------------------------------------
+
+// function evalObject(value) {
+//   var result = 0;
+//   switch(value.operation) {
+//     case '+': 
+//       result = value.a + value.b;
+//       break;
+//     case '-': 
+//       result = value.a - value.b;
+//       break;
+//     case '/': 
+//       result = value.a / value.b;
+//       break;
+//     case '*': 
+//       result = value.a * value.b;
+//       break;
+//     case '%': 
+//       result = value.a % value.b;
+//       break;
+//     case '^': 
+//       result = Math.pow(value.a, value.b);
+//       break;
+//   }
+//   return result;
+// }
+
+// const evalObject = value => {
+//   return {
+//     '+': v => v.a + v.b,
+//     '-': v => v.a - v.b,
+//     '/': v => v.a / v.b,
+//     '*': v => v.a * v.b,
+//     '%': v => v.a % v.b,
+//     '^': v => v.a ** v.b,
+//   }[value.operation](value);
+// }
+
+// console.log(evalObject({a: 5, b: 3, operation: '+'}));
+// console.log(evalObject({a: 5, b: 3, operation: '^'})); 
+
+// --------------------------------------------------------
+
+// function wrap(value) {
+//   return {
+//     value: value
+//   };
+// }
+
+// const wrapper_obj = wrap("my_wrapped_string");
+// console.log(wrapper_obj);
+// console.log(wrapper_obj["value"] === "my_wrapped_string");  
+
+// const wrap = value => ({value});
+
+// --------------------------------------------------------
