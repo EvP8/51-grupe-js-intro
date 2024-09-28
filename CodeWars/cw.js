5bc0acee948e79acd8cd7fb09260bf76a181462b
Evaldas Petkus <evaldas.db@gmail.com>
2024-09-28 22:33:28 +0300

    cw

--- CodeWars/cw.js
+++ CodeWars/cw.js
@@ -963,3 +963,56 @@
 // console.log(helloWorld);
 
 // --------------------------------------------------------
+
+// function pickIt(arr){
+//   var odd = [], even = [];
+  
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       even.push(arr[i]);
+//     } else {
+//       odd.push(arr[i]); 
+//     }
+//   }
+  
+//   return [odd, even];
+// }
+
+// console.log(pickIt([1, 2, 3, 4, 5])); 
+
+// function pickIt (arr) {
+  
+//   let odd = []
+//   let even =[]
+      
+//   for (var x of arr) {
+//       ((x % 2) ? odd : even).push(x)    
+//   }
+      
+//   return [odd, even]
+// }
+
+// -------------------------------------------------------
+
+// function padIt(str, n) {
+//   while (n > 0) {
+//     if (n % 2 !== 0) {
+//       str = '*' + str;
+//     } else {
+//       str = str + '*';
+//     }
+//     n--;
+//   }
+//   return str;
+// }
+
+// console.log(padIt());
+
+// function padIt(str, n) {
+//   while (n > 0) {
+//     str = n-- % 2 ? '*' + str : str + '*';
+//   }
+//   return str;
+// }
+
+// -------------------------------------------------------
